ea0cf28cb9f7973bbaf1261f12c84b98600ca6e3
Amitoj02 <40449197+Amitoj02@users.noreply.github.com>
2025-03-22 22:58:39 -0400

    feat: Base UI

---
+++ src/app/dialogs/settings/settings.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+
+    constructor() {
+
+    }
+}
